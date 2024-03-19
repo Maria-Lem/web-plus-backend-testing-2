3dc02db83c9588e912a9ad079ef9cdd72f02da87
Maria Alemaskina <maria131621@yandex.ru>
2024-03-19 16:41:20 +0300

    feat: add tests

--- src/posts.service.spec.ts
+++ src/posts.service.spec.ts
@@ -20,13 +20,53 @@ describe('PostsService', () => {
     });
 
     it('should return all posts if called without options', () => {
-      // реализуйте тест-кейс
+      const posts = postsService.findMany();
+
+      expect(posts.length).toBe(4);
     });
 
     it('should return correct posts for skip and limit options', () => {
-      // реализуйте тест-кейс
+      const posts = postsService.findMany({ skip: 1, limit: 2});
+
+      expect(posts.length).toEqual(2);
+      expect(posts[0]).toEqual({
+        text: 'Post 2',
+        id: '2',
+      });
+      expect(posts[1]).toEqual({
+        text: 'Post 3',
+        id: '3',
+      });
+    });
+  });
+
+  describe('.delete', () => {
+    it('should delete a post', () => {
+      const post = postsService.create({ text: 'Post will be deleted' });
+      postsService.delete(post.id);
+
+      const postNotFound = postsService.find(post.id);
+      expect(postNotFound).toEqual(undefined);
+    });
+  });
+
+  describe('.update', () => {
+    it('should update post', () => {
+      const post = postsService.create({ text: 'Post will be updated' });
+      postsService.update(post.id, { text: 'Updated post!' });
+
+      const updatedPost = postsService.find(post.id);
+
+      expect(updatedPost).toEqual({
+        id: '1',
+        text: 'Updated post!',
+      });
     });
 
-    // реализуйте недостающие тест-кейсы
+    it('should throw an error if post is not found', () => {
+      const post = postsService.create({ text: 'Post will be updated' });
+      
+      expect(() => postsService.update('wrong id', { text: 'Updated post' })).toThrow(new Error('Пост не найден'));
+    });
   });
 });
